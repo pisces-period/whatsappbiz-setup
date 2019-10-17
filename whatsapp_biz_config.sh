b1c1a310fada9b6be5df423ab9bccefd31d5746c
pisces-period <49252975+pisces-period@users.noreply.github.com>
2019-10-17 11:40:04 +0200

    adjust the hostname and port variables
    removing the " " pair from the variable place-holder.

--- whatsapp_biz_config.sh
+++ whatsapp_biz_config.sh
@@ -5,7 +5,7 @@
 # replace the variables wrapped around {{ }} 
 # OBS: The authorization header contains the base64 encoding for 'admin:secret' authentication
 
-curl -k -X POST https://"{{ HOSTNAME }}":"{{ PORT }}"/v1/users/login \
+curl -k -X POST https://{{ HOSTNAME }}:{{ PORT }}/v1/users/login \
   -w "%{http_code}\n" \
   -H 'Content-Type: application/json' \
   -H 'Authorization: Basic YWRtaW46c2VjcmV0' \
